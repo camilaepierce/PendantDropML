6b452207c3cc7276cf540ed791b303810293f359
camilaepierce <133548541+camilaepierce@users.noreply.github.com>
2025-06-19 15:18:46 +0200

    testing higher epochs, learning rate

--- utils/test_utils.py
+++ utils/test_utils.py
@@ -2,26 +2,5 @@ import matplotlib.pyplot as plt
 import numpy as np
 from skimage import io
 import torch
+import random
 
-
-def conv3d_grayscale_example(batch):
-    # for deterministic output only
-    torch.einsum()
-
-example = [
-    [
-        [255, 255, 255],
-        [0, 0, 0],
-        [255, 0, 255]
-    ]
-    [
-        [0, 0, 0],
-        [255, 255, 255],
-        [255, 0, 255]
-    ]
-    [
-        [255, 0, 255],
-        [255, 255, 255],
-        [0, 0, 0],
-    ]
-]
