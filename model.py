f69e71051f0e501781299d883fb085668d899a47
camilaepierce <133548541+camilaepierce@users.noreply.github.com>
2025-06-23 11:19:40 +0200

    running grayscale model optimization

--- model.py
+++ model.py
@@ -8,8 +8,8 @@ from torchvision import transforms
 # import torchvision.models as models
 import utils
 from utils.optimizer import run_optimizer
-from models.five_layer import FiveLayerCNN
-# from models.grayscaletransform import GrayscaleTransform
+# from models.five_layer import FiveLayerCNN
+from models.grayscaletransform import GrayscaleTransform
 from evaluation import evaluate_single, evaluate_directory
 
 
@@ -20,16 +20,16 @@ if __name__ == "__main__":
         config = load(jsonFile)
     
     # # Run the optimzer
-    # model = run_optimizer(config, GrayscaleTransform)
+    model = run_optimizer(config, GrayscaleTransform)
 
     # with open(config["save_info"]["modelName"] + ".txt", "a") as f:
     #     f.write(str(config))
     #     f.write([print(f"Layer: {name} | Size: {param.size()} | Values : {param[:2]} \n") for name, param in model.named_parameters()])
 
-    model = FiveLayerCNN()
-    model.load_state_dict(torch.load('model_weights/fiveLayerModelWeights.pth', weights_only=True))
+    # model = FiveLayerCNN()
+    # model.load_state_dict(torch.load('model_weights/fiveLayerModelWeights.pth', weights_only=True))
 
-    prediction = evaluate_single(model, "data/test_images/2083.png")
-    print(prediction)
+    # prediction = evaluate_single(model, "data/test_images/2083.png")
+    # print(prediction)
 
     # prediction_tensor = evaluate_directory(model, )
