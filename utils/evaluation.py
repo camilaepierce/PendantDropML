fixed bug of averaging stress tensor

utils/evaluation.py
@@ -47,9 +47,14 @@ def evaluate_directory(model, config_object, visualize=True, input_type="image")
     plt.clf()
     model.eval()
     data_paths = config_object["data_paths"]
+    isElastic = config_object["settings"]["isElastic"]
 
-    drop_dataset = PendantDropDataset(data_paths["params"], data_paths["rz"], data_paths["images"], data_paths["sigmas"], ignore_images=(input_type!="image"))
+    drop_dataset_dirty = PendantDropDataset(data_paths["params"], data_paths["rz"], data_paths["images"], data_paths["sigmas"], ignore_images=(input_type!="image"), clean_data=False)
 
+    drop_dataset = PendantDropDataset(data_paths["params"], data_paths["rz"], data_paths["images"], data_paths["sigmas"], ignore_images=(input_type!="image"), clean_data=True)
+
+    print("Set Difference")
+    print(drop_dataset.available_samples.difference(drop_dataset_dirty.available_samples))
     evaluation_info = []
     nan_samples = []
 
@@ -63,8 +68,9 @@ def evaluate_directory(model, config_object, visualize=True, input_type="image")
         G = features["Wo_Ar"]["Gmod"]
         frac = features["Wo_Ar"]["frac"]
 
-        if config_object["settings"]["isElastic"]:
+        if isElastic:
             sample_sigma = features["sigma_tensor"]
+            act_sigma = features["surface_tension"]
         else:
             sample_sigma = features["surface_tension"]
 
@@ -85,9 +91,12 @@ def evaluate_directory(model, config_object, visualize=True, input_type="image")
             true_diff_sq = np.square(true_diff)
             mse = np.average(true_diff_sq)
             relative_error = np.absolute(true_diff) / sample_sigma
-            #save info:: Wo, Ar, sigma, pred, true, relative, mse, K, G, frac
-            evaluation_info.append(np.array([Wo, Ar, np.average(sample_sigma), np.average(prediction), np.average(true_diff), np.average(relative_error), mse, K, G, frac]))
-
+            #save info:: Wo, Ar, act_sigma, avg_sigma, avg_pred, true, relative, mse, K, G, frac
+            if isElastic:
+                evaluation_info.append(np.array([Wo, Ar, np.average(sample_sigma), np.average(prediction), np.average(true_diff), np.average(relative_error), mse, K, G, frac, act_sigma]))
+            else:
+                #save info:: Wo, Ar, sigma, pred, true, relative, mse
+                evaluation_info.append(np.array([Wo, Ar, sample_sigma, prediction, true_diff, relative_error, mse]))
 
     #save data info to file
     evaluation_info = np.asarray(evaluation_info)
@@ -111,9 +120,11 @@ def evaluate_directory(model, config_object, visualize=True, input_type="image")
         all_true = evaluation_info[:, 4]
         all_rel = evaluation_info[:, 5]
         all_mse = evaluation_info[:, 6]
-        all_K = evaluation_info[:, 7]
-        all_G = evaluation_info[:, 8]
-        all_frac = evaluation_info[:, 9]
+        if isElastic:
+            all_K = evaluation_info[:, 7]
+            all_G = evaluation_info[:, 8]
+            all_frac = evaluation_info[:, 9]
+            all_act = evaluation_info[:, 10]
 
         with open(config_object["save_info"]["eval_results"] + "Distribution.txt", "a") as f:
             f.write("Sample Distribution\n")
@@ -132,10 +143,16 @@ def evaluate_directory(model, config_object, visualize=True, input_type="image")
         #                  yfull, yshort, 
         #                  cfull, cshort, 
         #                  config_object, norm=Normalize()):
-        scattershort(all_Wo, all_Ar, all_sigma, "viridis", 
+        scattershort(all_Wo, all_Ar, all_act, "viridis", 
                     "Worthington Number", "Wo",
                     "Aspect Ratio", "Ar",
-                    "Surface Tension", "SurfTension",
+                    "Actual Surface Tension", "AST",
+                    config_object, norm="log")
+        
+        scattershort(all_act, all_sigma, all_mse, "viridis", 
+                    "Surface Tension at Actual", "AST",
+                    "Average Surface Tension", "AvgST",
+                    "Mean Squared Error", "MSE",
                     config_object, norm="log")
         
         #plot Wo vs Ar vs accuracy
@@ -171,15 +188,15 @@ def evaluate_directory(model, config_object, visualize=True, input_type="image")
                     "Mean Squared Error", "MSE",
                     config_object, norm="log")
 
-        scattershort(all_K, all_sigma, all_mse, "magma", 
+        scattershort(all_K, all_act, all_mse, "magma", 
                     "K Modulus", "K",
-                    "Surface Tension at Apex", "SurfTens",
+                    "Surface Tension at Actual", "AST",
                     "Mean Squared Error", "MSE",
                     config_object, norm="log")
         
         #plot Surface tension vs accuracy
-        plt.scatter(all_sigma, all_true, c='forestgreen', marker=".")
-        plt.xlabel("Surface Tension")
+        plt.scatter(all_act, all_true, c='forestgreen', marker=".")
+        plt.xlabel("Surface Tension at Actual")
         plt.ylabel("Absolute Error")
         plt.title("Surface Tension vs Absolute Error")
         plt.savefig(config_object["save_info"]["eval_results"] + "SurfAccuracyTrue" + ".png")
@@ -187,7 +204,7 @@ def evaluate_directory(model, config_object, visualize=True, input_type="image")
         plt.show(block=False)
         plt.clf()
 
-        plt.scatter(all_sigma, all_rel, c="slateblue", marker=".")
+        plt.scatter(all_act, all_rel, c="slateblue", marker=".")
         plt.xlabel("Surface Tension")
         plt.ylabel("Relative Error")
         plt.title("Surface Tension vs Relative Error")
@@ -196,7 +213,7 @@ def evaluate_directory(model, config_object, visualize=True, input_type="image")
         plt.show(block=False)
         plt.clf()
 
-        plt.scatter(all_sigma, all_mse, c='forestgreen', marker=".")
+        plt.scatter(all_act, all_mse, c='forestgreen', marker=".")
         plt.xlabel("Surface Tension")
         plt.ylabel("Mean Squared Error")
         plt.title("Surface Tension vs MSE")
@@ -206,4 +223,36 @@ def evaluate_directory(model, config_object, visualize=True, input_type="image")
         plt.clf()
 
 
-        
+        ### Plotting with the inclusion of average surface tension ###
+
+        plt.scatter(all_act, all_true, c=all_sigma, norm=Normalize(), cmap='viridis', marker=".")
+        plt.xlabel("Surface Tension at Actual")
+        plt.ylabel("Absolute Error")
+        plt.title("Surface Tension vs Absolute Error")
+        plt.colorbar(label=f"Surface Tension Average")
+        plt.savefig(config_object["save_info"]["eval_results"] + "SurfAccuracyTrueAvgST" + ".png")
+
+        plt.show(block=False)
+        plt.clf()
+
+        plt.scatter(all_act, all_rel, c=all_sigma, norm=Normalize(), cmap='viridis', marker=".")
+        plt.xlabel("Surface Tension")
+        plt.ylabel("Relative Error")
+        plt.title("Surface Tension vs Relative Error")
+        plt.colorbar(label=f"Surface Tension Average")
+        plt.savefig(config_object["save_info"]["eval_results"] + "SurfAccuracyRelAvgST" + ".png")
+
+        plt.show(block=False)
+        plt.clf()
+
+        plt.scatter(all_act, all_mse, c=all_sigma, norm=Normalize(), cmap='viridis', marker=".")
+        plt.xlabel("Surface Tension")
+        plt.xscale("log")
+        plt.ylabel("Mean Squared Error")
+        plt.title("Surface Tension vs MSE")
+        plt.colorbar(label=f"Surface Tension Average")
+        plt.savefig(config_object["save_info"]["eval_results"] + "SurfAccuracyMSEAvgST" + ".png")
+
+        plt.show(block=False)
+        plt.clf()
+

utils/extraction.py
@@ -116,7 +116,7 @@ def get_digits_from_filename(filename):
 class PendantDropDataset(Dataset):
     """Pendant Drop Dataset, inherits from torch.utils.data.Dataset Class."""
 
-    def __init__(self, params_dir, rz_dir, img_dir, sigma_dir=None, transform=None, select_samples=None, ignore_images=False):
+    def __init__(self, params_dir, rz_dir, img_dir, sigma_dir=None, transform=None, select_samples=None, ignore_images=False, clean_data=False):
         """
         Some good documentation.
 
@@ -152,6 +152,9 @@ class PendantDropDataset(Dataset):
             self.available_samples = set(self.coord_outline_dict.keys())
         else:
             self.available_samples = select_samples.copy()
+        
+        if clean_data:
+            self.clean_data()
 
 
     def __len__(self):
@@ -233,6 +236,15 @@ class PendantDropDataset(Dataset):
             all_samples_combined.update(dataset.available_samples.copy())
 
         return PendantDropDataset(self.params_dir, self.rz_dir, self.img_dir, sigma_dir=self.sigma_dir, select_samples=all_samples_combined, ignore_images=self.ignore_images)
+    
+    def clean_data(self):
+        """
+        Removes drops that have a surface tension below 1.
+        """
+        for sample_id, surf_tens in self.surf_tens_dict.items():
+            if surf_tens < 1:
+                self.available_samples.remove(sample_id)
+
 
 # class SingleDropSimple:
 #     """
@@ -285,9 +297,28 @@ if __name__ == "__main__":
     ### Test Case for Dataset Class
     ##################################################
 
-    drop_dataset = PendantDropDataset("elastic_mini/test_data_params", "elastic_mini/test_data_rz","elastic_mini/test_images", "elastic_mini/test_data_sigmas", ignore_images=True)
+    dirty_data = PendantDropDataset("data/elastic_massive/test_data_params", "data/elastic_massive/test_data_rz","data/elastic_massive/test_images", "data/elastic_massive/test_data_sigmas", ignore_images=True, clean_data=False)
+
+    clean_data = PendantDropDataset("data/elastic_massive/test_data_params", "data/elastic_massive/test_data_rz","data/elastic_massive/test_images", "data/elastic_massive/test_data_sigmas", ignore_images=True, clean_data=True)
 
 
+    print(len(dirty_data))
+    print(len(clean_data))
+
+    print()
+    set_diff = dirty_data.available_samples.difference(clean_data.available_samples)
+    print(set_diff, "difference")
+
+    for sample in set_diff:
+        print(dirty_data[sample]["surface_tension"])
+
+
+    for i, sample_id in enumerate(dirty_data.available_samples):
+        tens = dirty_data[sample_id]["surface_tension"]
+        # if tens < 1:
+        print(sample_id, tens)
+        if i > 1000:
+            break
     # for i, sample_id in enumerate(drop_dataset.available_samples):
     #     sample = drop_dataset[sample_id]
 
@@ -297,5 +328,5 @@ if __name__ == "__main__":
 
     # print(drop_dataset["38"]["coordinates"])
 
-    print(type(drop_dataset["38"]["sigma_tensor"]))
-    print(drop_dataset["38"]["sigma_tensor"].shape)
+    # print(type(drop_dataset["38"]["sigma_tensor"]))
+    # print(drop_dataset["38"]["sigma_tensor"].shape)

utils/test_utils.py
@@ -1,27 +1,35 @@
 # import matplotlib.pyplot as plt
-from matplotlib.colors import Normalize
+# from matplotlib.colors import Normalize
 
-import numpy as np
-# from skimage import io
-import torch
+# import numpy as np
+# # from skimage import io
+# import torch
 # import random
+my_set = {"23", "1992", "2189", "210"}
+dictionary = {"23": 229, "1992": 2189, "210": 2190}
 
+print(my_set)
+for sample_id, value in dictionary.items():
+    print(sample_id, value)
+    if (value < 1000):
+        my_set.remove(sample_id)
 
+print(my_set)
 # with open("../results/evaluation/GandalfTheGrayEvaluation.txt", "r") as f:
 
-Wo,Ar,sample_sigma,prediction,abs_error,rel_error,mse,K, G, frac = np.loadtxt("results/evaluation/EmptyEvaluation.txt", delimiter=",", skiprows=1, unpack=True)
+# Wo,Ar,sample_sigma,prediction,abs_error,rel_error,mse,K, G, frac = np.loadtxt("results/evaluation/EmptyEvaluation.txt", delimiter=",", skiprows=1, unpack=True)
 
-print(np.min(mse), "minimum value of mse")
-print(np.max(mse), "maximum value of mse")
+# print(np.min(mse), "minimum value of mse")
+# print(np.max(mse), "maximum value of mse")
 
-norm = Normalize()
-normed_vals = norm(mse)
-print(np.min(normed_vals))
-print(np.max(normed_vals))
-print(norm.inverse(np.min(mse)))
-print(norm.inverse(np.max(mse)))
+# norm = Normalize()
+# normed_vals = norm(mse)
+# print(np.min(normed_vals))
+# print(np.max(normed_vals))
+# print(norm.inverse(np.min(mse)))
+# print(norm.inverse(np.max(mse)))
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 # plt.scatter(Wo, Ar, c=mse, norm=Normalize(), cmap="magma")
 # plt.colorbar(label="default norm")
@@ -85,9 +93,9 @@ import matplotlib.pyplot as plt
 # print(np.quantile(below_zero[:, 5], .50))
 # print(np.quantile(below_zero[:, 5], .75))
 
-print()
-print()
-print("data collection:")
-print("Average Relative Error:", np.average(rel_error))
-print("Average Absolute Error:", np.average(abs_error))
-print("Average MSE:", np.average(mse))
+# print()
+# print()
+# print("data collection:")
+# print("Average Relative Error:", np.average(rel_error))
+# print("Average Absolute Error:", np.average(abs_error))
+# print("Average MSE:", np.average(mse))